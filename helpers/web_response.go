14e06a3e399421cb80c3eb2198096b3e7743c6ca
pramudyas69 <18102280@ittelkom-pwt.ac.id>
2023-07-14 20:59:45 +0700

    Fixing Port

--- helpers/web_response.go
+++ helpers/web_response.go
@@ -1,7 +1,21 @@
 package helpers
 
+import (
+	"fmt"
+	"os"
+)
+
 type WebResponse struct {
 	Code   int         `json:"code"`
 	Status string      `json:"status"`
 	Data   interface{} `json:"data"`
+}
+
+func GetPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" // Default port if not specified
+	}
+
+	return fmt.Sprintf(":%s", port)
 }
