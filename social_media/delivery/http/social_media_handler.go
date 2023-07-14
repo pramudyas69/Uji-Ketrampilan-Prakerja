1a048de299a4809f9e12c8cdefbb2c762f13cc64
pramudyas69 <18102280@ittelkom-pwt.ac.id>
2023-07-14 20:38:40 +0700

    Add More FeatureSosmed

--- social_media/delivery/http/social_media_handler.go
+++ social_media/delivery/http/social_media_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strconv"
 	"uji/domain"
 	"uji/helpers"
 	"uji/middlewares"
@@ -23,11 +24,10 @@ func NewSosmedHandler(e *echo.Echo, sosmedUc domain.SosmedUseCase) {
 	{
 		router.Use(middlewares.Authentication)
 		router.POST("", handler.CreateSosmed)
-		//router.GET("/:Id", handler.GetUserById)
-		//router.GET("/", handler.GetUsers)
+		router.GET("", handler.GetSosmeds)
+		router.PUT("/:Id", handler.UpdateSosmed)
+		router.DELETE("/:Id", handler.DeleteUser)
 		//router.Use(middlewares.UserAuthorization(db))
-		//router.PUT("/:Id", handler.UpdateUser)
-		//router.DELETE("/:Id", handler.DeleteUser)
 	}
 }
 
@@ -63,5 +63,68 @@ func (h *SosmedHandler) CreateSosmed(ctx echo.Context) error {
 		Status: "CREATED",
 		Code:   201,
 		Data:   sosmed,
+	})
+}
+
+func (h *SosmedHandler) GetSosmeds(ctx echo.Context) error {
+	var sosmed []*domain.SocialMedia
+
+	res, err := h.sosmedUseCase.GetSosmedsUC(sosmed)
+	if err != nil {
+		return helpers.ErrorHandler(ctx, err)
+	}
+
+	return ctx.JSON(http.StatusOK, helpers.WebResponse{
+		Status: "OK",
+		Code:   200,
+		Data:   res,
+	})
+}
+
+func (h *SosmedHandler) UpdateSosmed(ctx echo.Context) error {
+	newSosmed := new(domain.SocialMedia)
+	id, _ := strconv.Atoi(ctx.Param("Id"))
+
+	if err := ctx.Bind(newSosmed); err != nil {
+		return ctx.JSON(http.StatusBadRequest, helpers.WebResponse{
+			Status: "BAD_REQUEST",
+			Code:   400,
+			Data:   "Json Payload Invalid!",
+		})
+	}
+
+	//_, err := govalidator.ValidateStruct(newSosmed)
+	//if err != nil {
+	//	return ctx.JSON(http.StatusBadRequest, helpers.WebResponse{
+	//		Status: "BAD_REQUEST",
+	//		Code:   400,
+	//		Data:   "Validation Error!",
+	//	})
+	//}
+
+	res, err := h.sosmedUseCase.UpdateSosmedUC(uint(id), newSosmed)
+	if err != nil {
+		return helpers.ErrorHandler(ctx, err)
+	}
+
+	return ctx.JSON(http.StatusOK, helpers.WebResponse{
+		Status: "OK",
+		Code:   200,
+		Data:   res,
+	})
+}
+
+func (h *SosmedHandler) DeleteUser(ctx echo.Context) error {
+	id, _ := strconv.Atoi(ctx.Param("Id"))
+
+	err := h.sosmedUseCase.DeleteSosmedUC(uint(id))
+	if err != nil {
+		return helpers.ErrorHandler(ctx, err)
+	}
+
+	return ctx.JSON(http.StatusOK, helpers.WebResponse{
+		Status: "OK",
+		Code:   200,
+		Data:   "Succesfull Deleted!",
 	})
 }
